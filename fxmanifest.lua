71c5ebccd0e0448937b45be7d0570013a8637d2a
CsakEgyWariiii <114402852+CsakEgyWariiii@users.noreply.github.com>
2023-05-21 14:48:27 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'ESX weathersync átírta Csak Egy Wari'
+version '2.1.0'
+
+shared_scripts {
+    '@es_extended/imports.lua',
+    'config.lua'
+}
+
+server_script 'server/server.lua'
+client_script 'client/client.lua'
+
+lua54 'yes'
